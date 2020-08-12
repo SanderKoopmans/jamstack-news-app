2cecca632bb9cca839a6bdfa9813a42d5eb48af8
Sander Koopmans <42515367+SanderKoopmans@users.noreply.github.com>
2020-08-12 14:59:32 +0200

    fetching mockdata

--- src/components/Newslist.jsx
+++ src/components/Newslist.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 function Newslist() {
+  fetch('../../mockData.json')
+    .then(res => res.json())
+    .then(data => console.log(data));
+
   return (
     <div className="newslist">
       <h1>Newslist</h1>
