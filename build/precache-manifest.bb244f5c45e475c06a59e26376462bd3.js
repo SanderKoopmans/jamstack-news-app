b0258a8b68a3b1feb2b912937d2ce8dd6fa80212
marc <10080192+mhlgoossens@users.noreply.github.com>
2020-08-13 11:02:18 +0200

    add react-bootstrap package, introduce simple styling

---
+++ build/precache-manifest.bb244f5c45e475c06a59e26376462bd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c97f42e5ededf69749dc281cb40562",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7512a03e43ee78ad9f7c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6d9abf2b15ea6fa06518",
+    "url": "/static/js/2.42d719e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42d719e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7512a03e43ee78ad9f7c",
+    "url": "/static/js/main.37f02aa5.chunk.js"
+  },
+  {
+    "revision": "82af36e83412180715d7",
+    "url": "/static/js/runtime-main.86c19a1b.js"
+  }
+]);
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -5,9 +5,14 @@ function Article(props) {
   const { title, description } = props;
 
   return (
-    <div className="Article">
-      <h2>{title}</h2>
-      <p>{description}</p>
+    <div className="card" style={{ width: '18rem' }}>
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        {/* <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6> */}
+        <p className="card-text">{description}</p>
+        {/* <a href="#" className="card-link">Card link</a>
+        <a href="#" className="card-link">Another link</a> */}
+      </div>
     </div>
   );
 }
